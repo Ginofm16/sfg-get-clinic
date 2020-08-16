87c1dffc0d40a8b68a59b211bfe36d237bb84e1e
gino <gino@gmail.com>
2020-08-16 18:30:55 -0500

    actualizando Owner, Perso, BaseEntity, Pet con JPA #12

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+/*esta anotacion estable esto como una clase base de JPA, en el que se indica
+* que esta clase va ser heredada por otras clases, y no se necesita especificar
+* esta clse asignada a la base de datos*/
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,8 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+/*este objecto no sera creado en la base de datos*/
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
