6418b8c4b5f5a8415ab9d32cad808c4e306f278f
gino <gino@gmail.com>
2020-08-17 21:57:21 -0500

    Actualizando profiles para JPA services #21

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -15,13 +15,16 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/", "/index", "/index.html"})
-    public String ListOwners(Model model){
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model){
 
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
 
-
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
 }
